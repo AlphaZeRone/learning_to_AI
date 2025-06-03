b696097f985f4e6968ec3cb13bb9bf33d4c8f44e
Nut <nuttasak.a03@gmail.com>
2025-06-03 21:25:08 +0700

    Make Schema test API with nbs

---
+++ FastAPI/Analytics_API/src/api/events/routing.py
@@ -0,0 +1,16 @@
+from fastapi import APIRouter
+from .schemas import EventSchema
+
+router = APIRouter()
+
+@router.get("/")
+def read_events():
+    #list of events
+    return {
+        "items": [1, 2, 3]
+    }
+
+@router.get("/{event_id}")
+def get_event(event_id: int) -> EventSchema:
+    # a single event
+    return {"id": event_id}
